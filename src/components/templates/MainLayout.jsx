94476ed69f085e38f166709e3c557e2a7ed4e49f
Duberney Gutierrez Bacca <dugutierrezbac@unal.edu.co>
2026-05-11 20:26:54 -0500

    feat: implement MainLayout

---
+++ src/components/templates/MainLayout.jsx
@@ -0,0 +1,14 @@
+import Header from '../organisms/Header';
+import Footer from '../organisms/Footer';
+
+export default function MainLayout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
