ad8d3e86f4853f37012707ca2acb2818c2587000
Duberney Gutierrez Bacca <dugutierrezbac@unal.edu.co>
2026-05-11 20:25:10 -0500

    feat: add users mockdata

---
+++ src/mockdata/users.js
@@ -0,0 +1,16 @@
+export const users = [
+  {
+    id: 1,
+    email: "admin@ecommerce.com",
+    password: "admin123",
+    name: "Admin User",
+    role: "admin"
+  },
+  {
+    id: 2,
+    email: "user@test.com",
+    password: "user123",
+    name: "Test User",
+    role: "user"
+  }
+];
